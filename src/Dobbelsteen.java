fc125c0778044df166c03782db4ae019ff5beaf5
henr0t <h.trieu@live.nl>
2020-06-24 17:06:34 +0200

    v0.7

---
+++ src/Dobbelsteen.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+class Dobbelsteen{
+int werpVast;
+int x;
+    int werpen(){
+        Random rnd = new Random();
+        x = rnd.nextInt(6)+1;
+        opslaan(x);
+        return x;
+
+    }
+
+    public String toString() {
+        return x + "";
+    }
+
+    void opslaan(int worp){
+        werpVast = worp;
+    }
+}
